4fd67a5dae5f704ed1a89061312c2333db4d1449
Icaro <icaromatoss@hotmail.com>
2026-05-21 01:38:27 -0300

    Adicionada validação de dados recebidos com Try/Catch

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+// Preparando o servidor para receber requisições JSON no corpo das requisições
+app.use(express.json());
+
+// Rota para obter todos os filmes
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -18,6 +22,31 @@ app.get('/movies', async (_, res) => {
     res.json(movies);
 });
 
+// Rota para criação de um novo filme (exemplo)
+app.post('/movies', async (req, res) => {
+    //Desestruturando os dados do corpo da requisição
+    const { title, release_date, genre_id, languages_id, oscar_count } =
+        req.body;
+
+    //Validação simples dos dados recebidos
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                release_date: new Date(release_date),
+                genre_id,
+                languages_id,
+                oscar_count,
+            },
+        });
+        return res.status(201).send({ message: 'Filme criado com sucesso' });
+    } catch (error) {
+        console.error('Erro ao criar filme:', error);
+        return res.status(500).send({ error: 'Erro ao criar o filme' });
+    }
+});
+
+// Porta onde o servidor irá escutar
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
