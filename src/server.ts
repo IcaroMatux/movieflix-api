2710a8437357f45763936d4d3b974d5ba11b9d8f
Icaro <icaromatoss@hotmail.com>
2026-05-27 05:37:44 -0300

    Adicionada rota de remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -24,18 +24,40 @@ app.get('/movies', async (_, res) => {
 
 // Rota para criação de um novo filme (exemplo)
 app.post('/movies', async (req, res) => {
-    //Desestruturando os dados do corpo da requisição
+    // Desestruturando os dados do corpo da requisição
     const { title, release_date, genre_id, languages_id, oscar_count } =
         req.body;
 
-    //Validação simples dos dados recebidos
+    // Validação simples dos dados recebidos
+    if (!title || typeof title !== 'string') {
+        return res.status(400).send({ error: 'Título do filme é obrigatório' });
+    }
+
+    const normalizedTitle = title.trim().toLowerCase();
+    if (!normalizedTitle) {
+        return res
+            .status(400)
+            .send({ error: 'Título do filme não pode ser vazio' });
+    }
+
     try {
+        // Verificando se já existe um filme com o mesmo título
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: normalizedTitle, mode: 'insensitive' } },
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send({
+                message: 'Já existe um filme cadastrado com esse título',
+            });
+        }
+
         await prisma.movie.create({
             data: {
-                title,
+                title: title.trim(),
                 release_date: new Date(release_date),
                 genre_id,
-                languages_id,
+                language_id: languages_id,
                 oscar_count,
             },
         });
@@ -44,6 +66,79 @@ app.post('/movies', async (req, res) => {
         console.error('Erro ao criar filme:', error);
         return res.status(500).send({ error: 'Erro ao criar o filme' });
     }
+});
+
+// Rota para atualizar um filme existente
+app.put('/movies/:id', async (req, res) => {
+    // pegar id do filme a ser atualizado
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ error: 'Filme não encontrado' });
+        }
+
+        const data = { ...req.body };
+
+        // Verificação se vier uma data de lançamento, converter para Data, se não, deixar como undefined para não atualizar esse campo
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+        console.log('Dados recebidos para atualização:', data);
+
+        //Atualizar o filme no banco de dados usando o Prisma
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+    } catch (error) {
+        console.error('Erro ao atualizar o filme:', error);
+        return res.status(500).send({ error: 'Erro ao atualizar o filme' });
+    }
+
+    // Retornar resposta de sucesso
+    res.status(200).send();
+});
+
+
+// Rota para deletar um filme existente
+app.delete('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+
+    // Tratamento de erros para garantir que o filme existe antes de tentar deletar e para lidar com possíveis erros durante a operação de exclusão
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        // Se o filme não for encontrado, retornar um erro 404 para indicar que o recurso não existe
+        if (!movie) {
+            return res.status(404).send({ error: 'Filme não encontrado' });
+        }
+
+        // Método do Prisma para deletar o filme do banco de dados usando o ID fornecido
+        await prisma.movie.delete({
+            where: {
+                id,
+            },
+        });
+        
+    } catch (error) {
+        // Caso dê algum erro no bloco try, como um erro de conexão com o banco de dados ou um erro inesperado, ele será capturado aqui e uma resposta de erro 500 será enviada para o cliente, indicando que houve um problema no servidor ao tentar processar a requisição
+        return res.status(500).send({ error: 'Erro ao deletar o filme' });
+    }
+    // Retornar resposta de sucesso
+    res.status(200).send();
 });
 
 // Porta onde o servidor irá escutar
